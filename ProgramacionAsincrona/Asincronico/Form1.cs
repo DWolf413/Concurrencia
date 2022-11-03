e1a8080c4bc1e731378d4c43def5631f315a2e76
David Logacho Torres <davidlogacho13@outlook.com>
2022-11-03 16:05:12 -0500

    Sincrincrono vs Asincrono

---
+++ ProgramacionAsincrona/Asincronico/Form1.cs
@@ -0,0 +1,27 @@
+namespace Asincronico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnIniciar_Click(object sender, EventArgs e)
+        {
+            //Version Sincrona
+            //Thread.Sleep(5000);
+
+            //Version Asincrona
+            loadingGIF.Visible = true;
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            loadingGIF.Visible = false;
+
+        }
+    }
+}
